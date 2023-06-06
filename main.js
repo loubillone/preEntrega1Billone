6662ab16d2d10697c86d59f33195e23b6aba7470
Lourdes Billone <lou.billone@gmail.com>
2023-06-05 23:07:39 -0300

    Primer entrega JS coder

---
+++ main.js
@@ -0,0 +1,49 @@
+alert("Bienvenido al sumulador de cuotas de Volkswagen");
+
+let vehiculo = prompt(
+  "Ingrese el nombre del vehículo a cotizar, en mayusculas: GOL TRENDLINE, POLO TRENDLINE O AMARAK TRENDLINE. \n Para salir ingrese ESC"
+);
+let precioGol = 1500600;
+let precioPolo = 2800600;
+let precioAmarok = 3500550;
+let cuotas = 84;
+
+while (
+  vehiculo !== "GOL TRENDLINE" &&
+  vehiculo !== "POLO TRENDLINE" &&
+  vehiculo !== "AMAROK TRENDLINE" &&
+  vehiculo !== "ESC"
+) {
+  alert("Vehículo inválido");
+  vehiculo = prompt(
+    "Ingrese el nombre del vehículo a cotizar, en mayusculas: GOL TRENDLINE, POLO TRENDLINE O AMARAK TRENDLINE"
+  );
+}
+
+if (vehiculo === "GOL TRENDLINE") {
+  alert(`Usted ingresó ${vehiculo}`);
+  alert("El precio de sus cuotas es $" + totalCuotasGol());
+} else if (vehiculo === "POLO TRENDLINE") {
+  alert(`Usted ingresó ${vehiculo}`);
+  alert("El precio de sus cuotas es $" + totalCuotasPolo());
+} else if (vehiculo === "AMAROK TRENDLINE") {
+  alert(`Usted ingresó ${vehiculo}`);
+  alert("El precio de sus cuotas es $" + totalCuotasAmarok());
+} else {
+  alert("Muchas gracias, lo esperamos pronto");
+}
+
+function totalCuotasGol() {
+  precioGolCuotas = precioGol / cuotas;
+  return parseInt(precioGolCuotas);
+}
+
+function totalCuotasPolo() {
+  precioPoloCuotas = precioPolo / cuotas;
+  return parseInt(precioPoloCuotas);
+}
+
+function totalCuotasAmarok() {
+  precioAmarokCuotas = precioAmarok / cuotas;
+  return parseInt(precioAmarokCuotas);
+}
